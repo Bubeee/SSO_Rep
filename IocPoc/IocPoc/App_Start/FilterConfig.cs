f58af7d354e68de7c587749f6639bfff5c87ddc4
Andrei_Krasnou <Andrei_Krasnou@epam.com>
2017-06-21 11:17:44 +0300

    Base for the POC, initital integration with Primary ADFS

---
+++ IocPoc/IocPoc/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IocPoc
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
